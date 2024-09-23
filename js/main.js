e5d98fe59d688a1c2334fd8729ca6c7dd504254b
rejaul25 <rejaulislammr25@gmail.com>
2024-09-24 00:10:02 +0600

    some functionality, sticky header added

--- js/main.js
+++ js/main.js
@@ -35,57 +35,82 @@ const buttons = document.querySelectorAll('.donateConfermBtn');
 const donateInputs = document.querySelectorAll('.donateInput');
 // history container
 const historyContainer = document.getElementById('history_container');
-// modal
-const modal = document.getElementById('my_modal_4');
-// console.log(buttons);
+
+// donation functionality
 for (const button of buttons) {
 
 
-    button.addEventListener('click', function (event) {
+    button.addEventListener('click', function () {
 
         const balanceNum = parseInt(myBalance.innerText);
         const inputField = button.previousElementSibling;
         const inputValue = parseInt(inputField.value);
 
-        if (inputValue < 0) {
-            alert("Opps! you entered negative value");
+        if (inputField.value === '') {
+            alert('please Enter the donate amount');
+            inputField.value = '';
             return;
-        }
-        else if (inputValue > balanceNum) {
+        } else if (inputValue <= 0) {
+            alert("Please Enter the positive value");
+            inputField.value = '';
+            return;
+        } else if (inputValue > balanceNum) {
             alert('you have not enough money');
+            inputField.value = '';
             return;
-        }
+        } else {
+            let newBalance = balanceNum - inputValue;
+            myBalance.innerText = newBalance;
 
-        let newBalance = balanceNum - inputValue;
-        myBalance.innerText = newBalance;
-
-        inputField.value = '';
-
-
-        // get donate for text
-        const nearCard = button.closest('.donate_card');
-        const donateFor = nearCard.querySelector('.donateFor');
-        const donateForText = donateFor.innerText;
-       
-        // get donate Status 
-        const donateStatus = nearCard.querySelector('.donateStatus');
-        const donateStatusValue = parseInt(donateStatus.innerText);
-        
-        donateStatus.innerText = donateStatusValue + inputValue;
+            inputField.value = '';
 
 
-        
-        
-        historyContainer.innerHTML += `
-         <div class="donate_history_card border-2 border-gray-200 mx-4 lg:mx-32 p-4 rounded-lg mt-4
+            // get donate for text
+            const nearCard = button.closest('.donate_card');
+            const donateFor = nearCard.querySelector('.donateFor');
+            const donateForText = donateFor.innerText;
+
+            // get donate Status 
+            const donateStatus = nearCard.querySelector('.donateStatus');
+            const donateStatusValue = parseInt(donateStatus.innerText);
+
+            donateStatus.innerText = donateStatusValue + inputValue;
+
+
+
+
+            historyContainer.innerHTML += `
+         <div class="donate_history_card w-full border-2 border-gray-200 mx-4 lg:mx-32 p-4 rounded-lg mt-4
                 
             ">
                     <h2 class="text-md md:text-xl font-semibold text-textPrimary"><span>${inputValue}</span> Taka is ${donateForText}</h2>
                     <p class="text-[16px] font-[300] text-textSecondary mt-2">Date : <span>${Date()}</span></p>
                 </div>
-        `
+        `;
 
-    
+            // modal
+            const modal = document.getElementById('my_modal_4');
+            modal.showModal();
+
+
+        }
+
     })
 
+}
+
+
+// header sticky
+
+const header = document.getElementById('header-main');
+const scrollAmount = 100;
+
+window.onscroll = function () {
+    if (window.scrollY > scrollAmount) {
+        header.classList.add('fixed', 'top-0', 'left-0', 'w-full', 'z-10');
+        header.classList.add('bg-bgPrimary', 'pb-10');
+    } else {
+        header.classList.remove('fixed', 'top-0', 'left-0', 'w-full', 'z-10');
+        header.classList.remove('bg-bgPrimary', 'pb-10');
+    }
 }
